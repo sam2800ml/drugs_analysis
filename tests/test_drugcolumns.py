8e49f1a90b4a14eafeac4e5409082d53e35c8e6e
Santiago <sam2800ml@gmail.com>
2025-03-27 18:37:46 -0500

    Error fixed on creatingid

--- tests/test_drugcolumns.py
+++ tests/test_drugcolumns.py
@@ -8,7 +8,7 @@ import pytest
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
-from dags.tasks.Transform import drug_columns
+from dags.tasks.Transform import creating_dateid
 
 
 def test_creating_dateid():
